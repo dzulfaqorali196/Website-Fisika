0bb052f338ae7e3b5073ff0de25dd539766ec102
Dzulfaqor Ali Dipangegara <whysoserius30@gmail.com>
2024-10-30 13:48:52 +0700

    menghilangkan credensial

--- frontend/js/client-auth/firebase-config.js
+++ frontend/js/client-auth/firebase-config.js
@@ -5,13 +5,7 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase
 
 // Konfigurasi Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAwU6p5oFB01B5g-y_DapcO4qZdIoShBWI",
-    authDomain: "labdz-virtual-lab.firebaseapp.com",
-    projectId: "labdz-virtual-lab",
-    storageBucket: "labdz-virtual-lab.appspot.com",
-    messagingSenderId: "633962376607",
-    appId: "1:633962376607:web:1e87044e12db41414ce1aa",
-    measurementId: "G-JCJPGKZKDM"
+    
 };
 
 // Initialize Firebase
